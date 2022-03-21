abed0ba90616d2ad8732972cf02df48b5413c535
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 18:23:13 -0400

    add search material ui icon, install material ui icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
-import './Header.css';
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
 
 const Header = () => {
   return (
     <div className="header">
+      <h1>header</h1>
       <div className="header__left">
         <img src="" alt="" />
       </div>  
       <div className="header__right">
+        <SearchIcon />
         <input type="text" />
       </div>
     </div>
