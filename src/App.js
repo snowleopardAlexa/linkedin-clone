aa56063291cd29f2fce004aa500f9d60f3050844
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 19:55:13 -0400

    create sidebar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Sidebar from './components/AppBody/Sidebar';
 import Header from './components/Header/Header';
 
 function App() {
   return (
+    <>
     <div className="app">
       <Header />
     </div>
+    <div className="app__body">
+      <Sidebar />
+    </div>
+    </>
   );
 }
 
