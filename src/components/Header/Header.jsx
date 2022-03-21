f4895afd689cc3bb5fbad7dbb1d6c3b7c5af187e
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 19:13:45 -0400

    add avatar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -25,6 +25,7 @@ const Header = () => {
         <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
         <HeaderOption Icon={ChatIcon} title="Messaging" />
         <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="/avatar.jpg" title="alexa" />
       </div>
     </div>
   )
