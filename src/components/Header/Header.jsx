ea3abc78227154f0494d58a645c79714f2aa70aa
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 18:56:35 -0400

    style header right, header, and header option

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import './Header.css'
 import SearchIcon from '@material-ui/icons/Search'
+import HeaderOption from './HeaderOption'
+import HomeIcon from '@material-ui/icons/Home'
+import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
 
 const Header = () => {
   return (
@@ -13,8 +16,9 @@ const Header = () => {
         <input type="text" />
       </div>
 
-      <div className="header-right">
-        
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
       </div>
     </div>
   )
