623f2b9b5c8c994d744950a59ebd64cf924276b2
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 18:17:31 -0400

    create Header component and Header style component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="header__left">
+        <img src="" alt="" />
+      </div>  
+      <div className="header__right">
+        <input type="text" />
+      </div>
+    </div>
+  )
+}
+
+export default Header
