5b39ffc26b93e37e752ffd972b17146cde36a6f0
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-21 20:02:52 -0400

    add sidebar jsx syntaxt

--- src/components/AppBody/Sidebar.jsx
+++ src/components/AppBody/Sidebar.jsx
@@ -1,8 +1,26 @@
-
+import { Avatar } from "@material-ui/core"
+import "./Sidebar.css"
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar">
+        <div className="sidebar__top">
+            <img src="" alt="background-image" />
+            <Avatar />
+            <h2>Alexa</h2>
+            <h4>aleksandravslomska@gmail.com</h4>
+        </div>
+        <div className="sidebar__stats">
+            <div className="sidebar_stat">
+               <p>Who viewed you</p>
+               <p className="sidebar__statNumber">2,345</p>
+            </div>
+            <div className="sidebar_stat">
+               <p>Whos on post</p>
+               <p className="sidebar__statNumber">2,345</p>
+            </div>
+        </div>
+    </div>
   )
 }
 
