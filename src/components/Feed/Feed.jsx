2baf32ccaf3430804da5482b25220a72fdbc0f2d
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-23 22:26:28 -0400

    add function sendPost - send data to firebase, add timestamp

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import './Feed.css'
 import CreateIcon from '@material-ui/icons/Create'
 import ImageIcon from '@material-ui/icons/Image'
@@ -7,14 +7,38 @@ import EventNoteIcon from '@material-ui/icons/EventNote'
 import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
 import InputOption from './Posts/InputOption/InputOption'
 import Posts from './Posts/Posts'
+import { db } from '../../firebase'
+import firebase from 'firebase'
 
 
 function Feed() {
 
+  const [input, setInput] = useState('')
   const [posts, setPosts] = useState([])
   
+  // push data to firebase
+  useEffect(() => {
+    db.collection("posts").onSnapshot(snapshot => (
+      setPosts(
+        snapshot.docs.map(doc => ({
+          id: doc.id,
+          data: doc.data(),
+        }
+      )))
+    ))
+  }, [])
+  
+  // send post to firebase
   const sendPost = e => {
      e.preventDefault()
+
+     db.collection("posts").add({
+       name: "Aleksandra Slomska",
+       description: "this is a test",
+       message: input,
+       photoUrl: '',
+       timestamp: firebase.firestore.FieldValue.serverTimestamp()
+     })
   }
 
   return (
@@ -23,7 +47,11 @@ function Feed() {
           <div className="feed__input">
             <CreateIcon />
             <form>
-                <input type="text" />
+                <input 
+                  value={input} 
+                  type="text"
+                  onChange={e => setInput(e.target.value)} 
+                />
                 <button onClick={sendPost} type="submit">Send</button>
             </form>
           </div>
@@ -51,7 +79,7 @@ function Feed() {
           </div>
       </div>
       {posts.map((post) => (
-       <Post />
+       <Posts />
       ))}
       <Posts 
         name="Aleksandra Slomska"
--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,6 @@
-// eslint-disable-next-line no-unused-vars
+/* eslint-disable no-unused-vars */
+import firebase from 'firebase'
+
 const firebaseConfig = {
     apiKey: "AIzaSyBVI34BvKzyW6JVsj0ZhxpoKCSCQFXtxtM",
     authDomain: "linkedin-clone-5ef14.firebaseapp.com",
@@ -7,3 +9,9 @@ const firebaseConfig = {
     messagingSenderId: "566464857746",
     appId: "1:566464857746:web:ef28268380ccd6876e884b"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig)
+  const db = firebaseApp.firestore()
+  const auth = firebase.auth()
+
+  export { db, auth }
