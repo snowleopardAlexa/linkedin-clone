c88034407d82b18550c17fec8f5c1388f103b461
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-23 22:05:41 -0400

    add firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+// eslint-disable-next-line no-unused-vars
+const firebaseConfig = {
+    apiKey: "AIzaSyBVI34BvKzyW6JVsj0ZhxpoKCSCQFXtxtM",
+    authDomain: "linkedin-clone-5ef14.firebaseapp.com",
+    projectId: "linkedin-clone-5ef14",
+    storageBucket: "linkedin-clone-5ef14.appspot.com",
+    messagingSenderId: "566464857746",
+    appId: "1:566464857746:web:ef28268380ccd6876e884b"
+  };
