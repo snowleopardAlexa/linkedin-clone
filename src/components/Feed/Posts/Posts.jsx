a1ea6ba77d3634fea80dac168847b1dad7a7dd4b
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-23 21:13:13 -0400

    add jsx syntaxt to Posts.jsx

--- src/components/Feed/Posts/Posts.jsx
+++ src/components/Feed/Posts/Posts.jsx
@@ -1,9 +1,19 @@
+import { Avatar } from '@material-ui/core'
 import './Posts.css'
 
-const Posts = () => {
+const Posts = ({ name, description, message, photoUrl }) => {
   return (
-    <div>
-      
+    <div className="posts">
+      <div className="post__header">
+          <Avatar />
+          <div className="post__info">
+              <h2>Aleksandra Slomska</h2>
+              <p>desc</p>
+          </div>
+      </div>
+      <div className="post__body">
+          <p>Message goes here</p>
+      </div>
     </div>
   )
 }
