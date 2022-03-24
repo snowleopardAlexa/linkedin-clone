8da3f07e2b84ecf3d3439fa5b3cc2986ba7d1d59
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-24 12:30:33 -0400

    add form login - name, email, passowrd, profile pic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css'
 import Sidebar from './components/AppBody/Sidebar'
 import Feed from './components/Feed/Feed'
 import Header from './components/Header/Header'
+import Login from './components/Login/Login'
 import { selectUser } from './features/userSlice'
 
 function App() {
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,20 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <div className="login"></div>
+    <div className="login">
+      <img 
+        src="/linkedin.png" 
+        alt="linkedin-logo" 
+      />
+
+      <form>
+        <input placeholder="Full name (required if registering)" type="text" />
+        <input placeholder="Profile pic URL (optional)" type="text" />
+        <input placeholder="Email" type="text" />
+        <input placeholder="Password" type="password" />
+        <button>Sign In</button>
+      </form>
+    </div>
   )
 }
 
