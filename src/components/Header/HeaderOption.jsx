7740aa3e308ec718c80ac9e3c69a3b38261648c9
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-24 14:21:24 -0400

    update name dynamically in Feed.jsx

--- src/components/Header/HeaderOption.jsx
+++ src/components/Header/HeaderOption.jsx
@@ -10,7 +10,9 @@ const user = useSelector(selectUser)
   return (
     <div onClick={onClick} className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
-      <Avatar className="headerOption__icon">{user?.email[0]}</Avatar>
+      {avatar && (
+        <Avatar className="headerOption__icon">{user?.email[0]}</Avatar>
+      )}
       <h3 className="headerOption__title">{title}</h3>
     </div>
   )
