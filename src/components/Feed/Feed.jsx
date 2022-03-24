f417cd6cb4c5e58f827888131717aba30277ebac
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-23 20:58:13 -0400

    add icons

--- src/components/Feed/Feed.jsx
+++ src/components/Feed/Feed.jsx
@@ -1,5 +1,10 @@
 import './Feed.css'
-import CreateIcon from "@material-ui/icons/Create"
+import CreateIcon from '@material-ui/icons/Create'
+import ImageIcon from '@material-ui/icons/Image'
+import InputOption from '@material-ui/icons/InputOption'
+import SubscriptionsIcon from '@material-ui/icons/Subscriptions'
+import EventNoteIcon from '@material-ui/icons/EventNote'
+import CalendarViewDayIcon from '@material-ui/icons/CalendarViewDay'
 
 function Feed() {
   return (
@@ -13,7 +18,26 @@ function Feed() {
             </form>
           </div>
           <div className="feed__inputOptions">
-            <InputOption />
+            <InputOption 
+              title="Photo" 
+              Icon={ImageIcon} 
+              color="#70B5F9"
+            />
+            <InputOption 
+              title="Photo" 
+              Icon={SubscriptionsIcon} 
+              color="#70B5F9"
+            />
+            <InputOption 
+              title="Event" 
+              Icon={EventNoteIcon} 
+              color="#70B5F9"
+            />
+            <InputOption 
+              title="Write article" 
+              Icon={CalendarViewDayIcon} 
+              color="#70B5F9"
+            />
           </div>
       </div>
     </div>
