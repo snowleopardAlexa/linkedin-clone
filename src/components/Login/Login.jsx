bab6708e318ddfc9735b1ce4ad66fce9479c5a2b
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-24 13:03:16 -0400

    add arrow function and value to input fields

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import './Login.css'
+import { auth } from './firebase'
 
 const Login = () => {
 
-const loginToApp = () => {}
+const [email, setEmail] = useState("")    
+const [password, setPassword] = useState("")
+const [name, setName] = useState("")
+const [profilePic, setProfilePic] = useState("")
 
-const register = () => {}
+const loginToApp = (e) => {
+    e.preventDefault();
+}
+
+const register = () => {
+   
+}
 
   return (
     <div className="login">
@@ -14,10 +25,34 @@ const register = () => {}
       />
 
       <form>
-        <input placeholder="Full name (required if registering)" type="text" />
-        <input placeholder="Profile pic URL (optional)" type="text" />
-        <input placeholder="Email" type="text" />
-        <input placeholder="Password" type="password" />
+        <input 
+          value={name} 
+          onChange={(e) => setName(e.target.value)}
+          type="text" 
+          placeholder="Full name (required if registering)"
+        />
+
+        <input 
+          value={profilePic} 
+          onChange={(e) => setProfilePic(e.target.value)} 
+          type="text" 
+          placeholder="Profile pic URL (optional)"
+        />
+
+        <input 
+          value={email} 
+          onChange={(e) => setEmail(e.target.value)} 
+          placeholder="Email" 
+          type="email" 
+        />
+
+        <input 
+           value={password} 
+           onChange={(e) => setPassword(e.target.value)}
+           placeholder="Password" 
+           type="password" 
+        />
+        
         <button type="submit" onClick={loginToApp}>Sign In</button>
       </form>
 
