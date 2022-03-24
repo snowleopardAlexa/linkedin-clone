1d874240321744eb0a3c7dc6527e18661ee95f13
snowleopardAlexa <alexaslomski@gmail.com>
2022-03-24 14:06:00 -0400

    get rid of dependency check in HeaderOption.jsx

--- src/components/Header/HeaderOption.jsx
+++ src/components/Header/HeaderOption.jsx
@@ -10,7 +10,7 @@ const user = useSelector(selectUser)
   return (
     <div onClick={onClick} className="headerOption">
       {Icon && <Icon className="headerOption__icon" />}
-      {avatar && <Avatar className="headerOption__icon" src={user?.photoUrl}>{user?.email[0]}</Avatar>}
+      <Avatar className="headerOption__icon">{user?.email[0]}</Avatar>
       <h3 className="headerOption__title">{title}</h3>
     </div>
   )
